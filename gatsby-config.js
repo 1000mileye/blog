9004d91fbe8463bb2049862b35f14049c2ef27bc
koro <40223073+koroomoto@users.noreply.github.com>
2018-06-15 16:22:19 +0900

    create blog by gatsby starter

---
+++ gatsby-config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  siteMetadata: {
+    title: `Blog powerd by gatsbyjs`,
+  },
+  plugins: [
+    `gatsby-plugin-react-helmet`,
+    // Add typescript stack into webpack
+    `gatsby-plugin-typescript`,
+  ],
+};
